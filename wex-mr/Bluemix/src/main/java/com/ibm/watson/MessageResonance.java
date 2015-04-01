7b6f304eba060fbd1092d88b86b8e991cef1b14e
Michael Keeling <mkeeling@us.ibm.com>
2015-04-01 15:44:05 -0400

    Updated build system to use Maven, removed dependency on WLP JAX-RS library

--- wex-mr/Bluemix/src/main/java/com/ibm/watson/MessageResonance.java
+++ wex-mr/Bluemix/src/main/java/com/ibm/watson/MessageResonance.java
@@ -32,16 +32,16 @@ import org.apache.wink.json4j.JSONObject;
 
 @javax.ws.rs.ApplicationPath("resources")
 @Path("/resonate")
-public class MessageResonance extends javax.ws.rs.core.Application {
-
+public class MessageResonance extends javax.ws.rs.core.Application
+{
 	@Context
 	private UriInfo context;
 
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getMessageResonance(@FormParam("message") String message)
-			throws Exception {
+	public Response getMessageResonance(@FormParam("message") String message) throws Exception
+	{
 
 		String userid = null;
 		String password = null;
@@ -54,11 +54,10 @@ public class MessageResonance extends javax.ws.rs.core.Application {
 		String Service_Name = "message_resonance";
 
 		// Get the Service Credentials for Watson Message Resonance
-		if (VCAP_SERVICES != null
-				&& ((JSONObject) JSON.parse(VCAP_SERVICES))
-						.containsKey(Service_Name)) {
-
-			try {
+		if (VCAP_SERVICES != null && ((JSONObject) JSON.parse(VCAP_SERVICES)).containsKey(Service_Name))
+		{
+			try
+			{
 				JSONObject obj = (JSONObject) JSON.parse(VCAP_SERVICES);
 				JSONArray service = obj.getJSONArray(Service_Name);
 
@@ -74,32 +73,32 @@ public class MessageResonance extends javax.ws.rs.core.Application {
 				endpointBaseURI = credentials.getString("url");
 				JSONArray services = credentials.getJSONArray("services");
 				JSONObject ringObject = services.getJSONObject(0);
-				ringScoreURI = endpointBaseURI + "/" + ringObject.getJSONObject("ringscore").getString(
-						"path");
-				datasetURI = endpointBaseURI + "/" + ringObject.getJSONObject("datasets").getString(
-						"path");
+				ringScoreURI = endpointBaseURI + "/" + ringObject.getJSONObject("ringscore").getString("path");
+				datasetURI = endpointBaseURI + "/" + ringObject.getJSONObject("datasets").getString("path");
 
-			} catch (NullPointerException | JSONException e) {
+			}
+
+			catch (Exception e)
+			{
 				e.printStackTrace();
 			}
-		} else {
+		}
+		else
+		{
 			// add logging
-			System.out
-					.println("VCAP_SERVICES does not contain the Watson Service. Services are probably not associated with this application.");
+			System.out.println("VCAP_SERVICES does not contain the Watson Service. Services are probably not associated with this application.");
 			// generic error condition
-			return Response
-					.status(Response.Status.BAD_REQUEST)
-					.header("Pragma", "no-cache")
-					.header("Cache-Control", "no-cache")
-					.entity("{\"error\" : \"Watson service information was not found. Is the Watson Message Resonance service associated with this application?\"}")
-					.build();
+			return Response.status(Response.Status.BAD_REQUEST)
+										 .header("Pragma", "no-cache")
+										 .header("Cache-Control", "no-cache")
+										 .entity("{\"error\" : \"Watson service information was not found. Is the Watson Message Resonance service associated with this application?\"}")
+										 .build();
 		}
 
 		// create credential information for security this contains the
 		// information returned by the service
 		CredentialsProvider provider = new BasicCredentialsProvider();
-		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(
-				userid, password);
+		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(userid, password);
 		provider.setCredentials(AuthScope.ANY, credentials);
 
 		// assume a self signed certificate which doesn't have a host name that
@@ -107,28 +106,28 @@ public class MessageResonance extends javax.ws.rs.core.Application {
 		// Fix on service deployment
 		SSLContextBuilder builder = new SSLContextBuilder();
 		builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-				builder.build(),
-				SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build(),
+																																			SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
 		CloseableHttpClient client = HttpClients.custom()
-				.setSSLSocketFactory(sslsf)
-				.setDefaultCredentialsProvider(provider).build();
+																						.setSSLSocketFactory(sslsf)
+																					 	.setDefaultCredentialsProvider(provider).build();
 
 		StringBuilder resultString = new StringBuilder();
 
-		if (message == null || message.isEmpty()) {
+		if (message == null || message.isEmpty())
+		{
 			resultString.append("{\"error\": \"No words to evaluate!\"}");
-		} else {
-
+		}
+		else
+		{
 			// Split the string on blocks of non-alphanumeric (by unicode standards) characters
 			String[] words = message.split("(?U)\\W+");
-
+		
 			resultString.append("{\"results\":[");
-			for (String word : words) {
 
+			for (String word : words) {
 				// create the HTTP Get operation
-				HttpGet httpGet = new HttpGet(ringScoreURI + "?dataset=1&text="
-						+ java.net.URLEncoder.encode(word, "UTF-8"));
+				HttpGet httpGet = new HttpGet(ringScoreURI + "?dataset=1&text=" + java.net.URLEncoder.encode(word, "UTF-8"));
 
 				HttpResponse response = client.execute(httpGet);
 
@@ -136,54 +135,54 @@ public class MessageResonance extends javax.ws.rs.core.Application {
 				System.out.println("This is the return string: " + serviceResponseString);
 
 				JSONObject serviceResponseJson = new JSONObject(serviceResponseString);
-				boolean haveError = serviceResponseJson.containsKey("error")
-						|| !serviceResponseJson.containsKey("overall");
-				if (haveError) {
+				boolean haveError = serviceResponseJson.containsKey("error") || !serviceResponseJson.containsKey("overall");
+				if (haveError)
+				{
 					System.out.println("Error detected in service response ----------------  " );
 					System.out.println(serviceResponseString);
-					resultString = new StringBuilder(
-							"{\"error\": \"Service Error on " + word + "\"}");
+					resultString = new StringBuilder("{\"error\": \"Service Error on " + word + "\"}");
 				
 					return Response.status(Response.Status.ACCEPTED)
-							.header("Pragma", "no-cache")
-							.header("Cache-Control", "no-cache")
-
-							.entity(resultString.toString()).build();
-				} else {
-					int score = (int) serviceResponseJson.get("overall");
+												 .header("Pragma", "no-cache")
+												 .header("Cache-Control", "no-cache")
+												 .entity(resultString.toString()).build();
+				}
+				else
+				{
+					int score = (Integer) serviceResponseJson.get("overall");
 					String calculateColorCode = calculateColorCode(score);
 					resultString.append("{");
 					resultString.append("\"word\":\"" + word + "\",");
 					resultString.append("\"score\":\"" + score + "\",");
-					resultString.append("\"color\":\"" + calculateColorCode
-							+ "\"");
+					resultString.append("\"color\":\"" + calculateColorCode + "\"");
 					resultString.append("},");
 				}
 
 			}
 			if (resultString.lastIndexOf(",") > 0)
+			{
 				resultString.deleteCharAt(resultString.lastIndexOf(","));
-			resultString.append("]}");
+			}
 
+			resultString.append("]}");
 		}
 
 		// return the response
 		return Response.status(Response.Status.ACCEPTED)
-				.header("Pragma", "no-cache")
-				.header("Cache-Control", "no-cache")
-
-				.entity(resultString.toString()).build();
-
+									 .header("Pragma", "no-cache")
+									 .header("Cache-Control", "no-cache")
+   								 .entity(resultString.toString()).build();
 	}
 
-	private String calculateColorCode(int scoreInt) {
-		if (scoreInt < 11)
+	private String calculateColorCode(int score)
+	{
+		if (score < 11)
 			return "black";
-		else if (scoreInt < 21)
+		else if (score < 21)
 			return "cornflowerblue";
-		else if (scoreInt < 31)
+		else if (score < 31)
 			return "lawngreen";
-		else if (scoreInt < 41)
+		else if (score < 41)
 			return "goldenrod";
 		else
 			return "crimson";
